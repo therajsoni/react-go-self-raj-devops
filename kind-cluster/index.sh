83908fe5ced50871ec20efa64f13c8629139629e
therajsoni <rajsoni968392@gmail.com>
2026-09-24 11:44:58 +0530

    First Push

---
+++ kind-cluster/index.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y docker.io
+sudo systemctl enable --now docker
+docker --version
+sudo usermod -aG docker $USER
+newgrp docker
